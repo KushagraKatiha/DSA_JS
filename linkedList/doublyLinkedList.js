7f8d4438c177d0489b64b9434c228e7074a23255
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-06-07 21:35:03 +0530

    doubly linked list-insert at start

--- linkedList/doublyLinkedList.js
+++ linkedList/doublyLinkedList.js
@@ -12,3 +12,17 @@ class DoublyLinkedList{
         this.prev = null
     }
 }
+
+DoublyLinkedList.prototype.insertAtBegning = function(data){
+    const newNode = new Node(data, null, this.head)
+
+    if(!this.head){
+        this.head.prev = newNode
+    }
+
+    this.head = newNode
+    this.head = newNode
+    if(this.tail === null){
+        this.tail = newNode
+    }
+}
